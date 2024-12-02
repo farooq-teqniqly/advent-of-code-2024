4b8dc9afa97f308be3b96a58810b57a26e49719d
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2024-12-02 10:10:00 -0800

    refactor: refactor solve logic into a Strategy.

--- src/AdventOfCode2024.Lib/Day01Puzzle.cs
+++ src/AdventOfCode2024.Lib/Day01Puzzle.cs
@@ -1,30 +1,40 @@
-﻿namespace AdventOfCode2024.Lib;
+namespace AdventOfCode2024.Lib;
 
 public class Day01Puzzle
 {
-    public static int Solve(string dataFilePath)
+    public interface ISolveStrategy
     {
-        var lines = File.ReadLines(dataFilePath);
-        var leftQueue = new PriorityQueue<int, int>();
-        var rightQueue = new PriorityQueue<int, int>();
-
-        foreach (var line in lines)
-        {
-            var parts = line.Split("   ").Select(int.Parse).ToArray();
-            var leftValue = parts[0];
-            var rightValue = parts[1];
-
-            leftQueue.Enqueue(leftValue, leftValue);
-            rightQueue.Enqueue(rightValue, rightValue);
-        }
-
-        var totalDistance = 0;
-
-        while (leftQueue.Count > 0 && rightQueue.Count > 0)
-        {
-            totalDistance += Math.Abs(leftQueue.Dequeue() - rightQueue.Dequeue());
-        }
-
-        return totalDistance;
+        int Solve(string dataFilePath);
     }
+
+    public class PriorityQueueSolveStrategy : ISolveStrategy
+    {
+        public int Solve(string dataFilePath)
+        {
+            var lines = File.ReadLines(dataFilePath);
+            var leftQueue = new PriorityQueue<int, int>();
+            var rightQueue = new PriorityQueue<int, int>();
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split("   ").Select(int.Parse).ToArray();
+                var leftValue = parts[0];
+                var rightValue = parts[1];
+
+                leftQueue.Enqueue(leftValue, leftValue);
+                rightQueue.Enqueue(rightValue, rightValue);
+            }
+
+            var totalDistance = 0;
+
+            while (leftQueue.Count > 0 && rightQueue.Count > 0)
+            {
+                totalDistance += Math.Abs(leftQueue.Dequeue() - rightQueue.Dequeue());
+            }
+
+            return totalDistance;
+        }
+    }
+
+    public static int Solve(ISolveStrategy solveStrategy, string dataFilePath) => solveStrategy.Solve(dataFilePath);
 }
--- test/AdventOfCode2024.Tests/Day01Tests.cs
+++ test/AdventOfCode2024.Tests/Day01Tests.cs
@@ -6,10 +6,11 @@ namespace AdventOfCode2024.Tests;
 public class Day01Tests
 {
     [Fact]
-    public void Can_Get_The_Total_Distance()
+    public void Can_Get_The_Total_Distance_Using_Priority_Queeu()
     {
         var dataFilePath = Path.Join(Directory.GetCurrentDirectory(), "../../../../../", "data", "day01.txt");
-        var distance = Day01Puzzle.Solve(dataFilePath);
+        var strategy = new Day01Puzzle.PriorityQueueSolveStrategy();
+        var distance = Day01Puzzle.Solve(strategy, dataFilePath);
 
         distance.Should().Be(1_579_939);
     }
